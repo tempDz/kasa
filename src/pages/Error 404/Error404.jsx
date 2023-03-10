220f083cf9e00aab85f7a3636e011a6ea1d3dcab
Adel <temp.openclassrooms@gmx.fr>
2023-03-10 02:16:16 +0100

    finalisation forme Error404

--- src/pages/Error 404/Error404.jsx
+++ src/pages/Error 404/Error404.jsx
@@ -1,29 +1,47 @@
 import React from 'react';
 import styled from 'styled-components';
+import { Link } from 'react-router-dom';
 
 const Container = styled.div`
-  height: 100vh;
+  height: 700px;
   display: flex;
-  justify-content: center;
+  flex-direction: column;
   align-items: center;
+  justify-content: flex-start;
   font-family: 'Montserrat';
+  font-style: normal;
+  color: #FF6060;
 `;
 
 const Text = styled.h1`
-  font-style: normal;
-  font-weight: 700;
+  margin: 0;
   font-size: 288px;
   line-height: 142.6%;
-  display: flex;
-  align-items: center;
-  text-align: center;
+`;
+
+const OopsMessage = styled.h2`
+  font-weight: 500;
+  font-size: 36px;
+  line-height: 142.6%;
+  align-items: flex-end;
+  margin: 0;
+`;
+
+const RetourAccueil = styled(Link)`
+  font-size: 18px;
+  line-height: 142.6%;
+  margin-top: 150px;
   color: #FF6060;
+  text-decoration: none;
+  border-bottom: 2px solid #FF6060;
 `;
 
 function Error404() {
   return (
     <Container>
       <Text>404</Text>
+      <OopsMessage>Oups! La page que vous demandez n'existe pas.</OopsMessage>
+      <RetourAccueil to="/">Retourner sur la page d’accueil</RetourAccueil>
     </Container>
   );
 }
