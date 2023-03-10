febc44036cf59a4614f4358b900b22e7aec07044
Adel <temp.openclassrooms@gmx.fr>
2023-03-10 17:46:07 +0100

    séparation du code carousel et Accordion

--- src/components/Carousel/Carousel.jsx
+++ src/components/Carousel/Carousel.jsx
@@ -1,10 +1,7 @@
 import React, { useState, useEffect, useRef } from "react";
 import styled from "styled-components";
 import arrowImage from '../../assets/fleche carousel.png';
-import star from '../../assets/star.png';
-import starGray from '../../assets/star_gray.png';
 import Data from '../../Data/Data';
-import Accordion from "../Accordion/Accordion";
 
 const CarouselContainer = styled.div`
   min-height: 550px;
@@ -62,113 +59,6 @@ const CarouselImage = styled.img`
   position: absolute;
 `;
 
-const InfoContainer = styled.div`
-  display: flex;
-  flex-direction: row;
-  justify-content: space-between;
-  font-weight: bold;
-`;
-
-const InfoLogement = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: flex-start;
-  padding: 20px 0 20px 0;
-  font-family: 'Montserrat';
-  font-style: normal;
-  font-weight: 500;
-  color: #FF6060;
-  line-height: 142.6%;
-`;
-
-const InfoTitreLogement = styled.div`
-  font-size: 36px;
-  padding-bottom:20px;
-`;
-
-const InfoDescriptifLogement= styled.div`
-font-size: 18px;
-padding-bottom:20px;
-`;
-
-const TagsContainer = styled.div`
-  display: flex;
-  flex-wrap: wrap;
-`;
-
-const Tag = styled.div`
-font-family: 'Montserrat';
-  font-style: normal;
-  font-weight: 500;
-  font-size: 14px;
-  line-height: 142.6%;
-  text-align: center;
-  color: #FFFFFF;
-  background: #FF6060;
-  Width:auto;
-  border-radius: 10px;
-  padding: 5px 10px;
-  margin-right: 10px; 
-`;
-
-const RateLogement = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: space-between;
-  align-items: center;
-  padding: 20px 0 20px 0;
-`;
-
-const NameContainer = styled.div`
-  display: flex;
-  align-items: center;
-`;
-
-const Name = styled.div`
-font-family: 'Montserrat';
-font-style: normal;
-font-weight: 500;
-font-size: 18px;
-line-height: 142.6%;
-  color: #FF6060;
-`;
-
-const Picture = styled.img`
-  width: 64px;
-  height: 64px;
-  object-fit: cover;
-  border-radius: 50%;
-  border: 1px solid #ccc;
-  margin-left: 20px;
-`;
-
-const RatingContainer = styled.div`
-  display: flex;
-  align-items: center;
-`;
-
-const Rating = ({ rating }) => {
-  const fullStars = Math.floor(rating);
-  const emptyStars = 5 - fullStars;
-
-  return (
-    <>
-      {[...Array(fullStars)].map((_, i) => (
-        <img key={i} src={star} alt="star" />
-      ))}
-      {[...Array(emptyStars)].map((_, i) => (
-        <img key={i} src={starGray} alt="empty star" />
-      ))}
-    </>
-  );
-};
-
-const AccordionContainer = styled.div`
-  display: flex;
-  justify-content: space-between;
-  margin-top: 50px;
-`;
-const accordionWidths = [45];
 
 function Carousel(props) {
   const [currentIndex, setCurrentIndex] = useState(0);
@@ -220,42 +110,6 @@ function Carousel(props) {
           <CarouselArrowRight onClick={handleClickNext} />
         </CarouselArrowContainer>
       </CarouselImageContainer>
-      <InfoContainer>
-        <InfoLogement>
-        <InfoTitreLogement>
-          <div>{apartment.title}</div>
-          </InfoTitreLogement>
-          <InfoDescriptifLogement>
-          <div>{apartment.location}</div>
-          </InfoDescriptifLogement>
-          <TagsContainer>
-            {apartment.tags.map((tag, index) => (
-              <Tag key={index}>{tag}</Tag>
-            ))}
-          </TagsContainer>
-        </InfoLogement>
-        <RateLogement>
-          <NameContainer>
-            <Name>{apartment.host.name}</Name>
-            <Picture src={apartment.host.picture} alt={apartment.host.name} />            
-          </NameContainer>
-          <RatingContainer>
-            <Rating rating={apartment.rating} />
-          </RatingContainer>
-        </RateLogement>
-            </InfoContainer>
-            <AccordionContainer>
-        <Accordion title="Description" width={accordionWidths[0]}>
-        <p>
-              {apartment.description}
-            </p>
-            </Accordion>
-          <Accordion title="Équipements" width={accordionWidths[0]}>
-          {apartment.equipments.map((item, index) => (
-    <p key={index}>{item}</p>
-  ))}
-          </Accordion>
-        </AccordionContainer>
     </CarouselContainer>
   );
 }
--- src/pages/Fiche Logement/FicheLogement.jsx
+++ src/pages/Fiche Logement/FicheLogement.jsx
@@ -1,22 +1,190 @@
 import { useEffect, useState } from "react";
 import { useLocation } from 'react-router-dom';
+import styled from "styled-components";
 import Carousel from '../../components/Carousel/Carousel';
+import star from '../../assets/star.png';
+import starGray from '../../assets/star_gray.png';
+import Accordion from "../../components/Accordion/Accordion";
+import Data from '../../Data/Data';
+
+const FicheLogementContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  width:90%;
+  max-width: 1240px;
+  margin: auto;
+`;
+
+const InfoContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  font-weight: bold;
+`;
+
+const InfoLogement = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  padding: 20px 0 20px 0;
+  font-family: 'Montserrat';
+  font-style: normal;
+  font-weight: 500;
+  color: #FF6060;
+  line-height: 142.6%;
+`;
+
+const InfoTitreLogement = styled.div`
+  font-size: 36px;
+  padding-bottom:20px;
+`;
+
+const InfoDescriptifLogement= styled.div`
+  font-size: 18px;
+  padding-bottom:20px;
+`;
+
+const TagsContainer = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+`;
+
+const Tag = styled.div`
+  font-family: 'Montserrat';
+  font-style: normal;
+  font-weight: 500;
+  font-size: 14px;
+  line-height: 142.6%;
+  text-align: center;
+  color: #FFFFFF;
+  background: #FF6060;
+  Width:auto;
+  border-radius: 10px;
+  padding: 5px 10px;
+  margin-right: 10px; 
+`;
+
+const RateLogement = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+  padding: 20px 0 20px 0;
+`;
+
+const NameContainer = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const Name = styled.div`
+  font-family: 'Montserrat';
+  font-style: normal;
+  font-weight: 500;
+  font-size: 18px;
+  line-height: 142.6%;
+  color: #FF6060;
+`;
+
+const Picture = styled.img`
+  width: 64px;
+  height: 64px;
+  object-fit: cover;
+  border-radius: 50%;
+  border: 1px solid #ccc;
+  margin-left: 20px;
+`;
+
+const RatingContainer = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const Rating = ({ rating }) => {
+  const fullStars = Math.floor(rating);
+  const emptyStars = 5 - fullStars;
+
+  return (
+    <>
+      {[...Array(fullStars)].map((_, i) => (
+        <img key={i} src={star} alt="star" />
+      ))}
+      {[...Array(emptyStars)].map((_, i) => (
+        <img key={i} src={starGray} alt="empty star" />
+      ))}
+    </>
+  );
+};
+
+const AccordionContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  margin-top: 50px;
+`;
+
+const accordionWidths = [45];
 
 function FicheLogement() {
-  const [apartmentId, setApartmentId] = useState(null);
+  const [apartment, setApartment] = useState(null);
   const location = useLocation();
 
   useEffect(() => {
     const searchParams = new URLSearchParams(location.search);
     const id = searchParams.get("id");
-    setApartmentId(id);
+    const foundApartment = Data.find(item => item.id === id);
+    if (foundApartment) {
+      setApartment(foundApartment);
+    } else {
+      console.error(`Appartement avec l'ID ${id} non trouvé`);
+    }
   }, [location.search]);
+
+  if (!apartment) {
+    return <div>Loading...</div>;
+  }
 
   return (
     <>
-      {apartmentId && <Carousel apartmentId={apartmentId} />}
+      <Carousel apartmentId={apartment.id} />
+      <FicheLogementContainer>
+      <InfoContainer>
+        <InfoLogement>
+          <InfoTitreLogement>
+            <div>{apartment.title}</div>
+          </InfoTitreLogement>
+          <InfoDescriptifLogement>
+            <div>{apartment.location}</div>
+          </InfoDescriptifLogement>
+          <TagsContainer>
+            {apartment.tags.map((tag, index) => (
+              <Tag key={index}>{tag}</Tag>
+            ))}
+          </TagsContainer>
+        </InfoLogement>
+        <RateLogement>
+          <NameContainer>
+            <Name>{apartment.host.name}</Name>
+            <Picture src={apartment.host.picture} alt={apartment.host.name} />
+          </NameContainer>
+          <RatingContainer>
+            <Rating rating={apartment.rating} />
+          </RatingContainer>
+        </RateLogement>
+      </InfoContainer>
+      <AccordionContainer>
+        <Accordion title="Description" width={accordionWidths[0]}>
+          <p>{apartment.description}</p>
+        </Accordion>
+        <Accordion title="Équipements" width={accordionWidths[0]}>
+          {apartment.equipments.map((item, index) => (
+            <p key={index}>{item}</p>
+          ))}
+        </Accordion>
+      </AccordionContainer>
+      </FicheLogementContainer>
     </>
   );
 }
+
 
 export default FicheLogement;
